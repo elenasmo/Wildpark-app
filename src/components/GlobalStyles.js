74a170eae4fd3de6bc5fc55d56930334cb177e75
elenasmo <elena.smo@web.de>
2019-09-30 15:05:47 +0200

    Add animal-cards

---
+++ src/components/GlobalStyles.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components'
+
+const GlobalStyle = createGlobalStyle`
+* {
+    box-sizing: border-box;
+}
+body {
+    margin: 0;
+   
+}
+`
+export default GlobalStyle
