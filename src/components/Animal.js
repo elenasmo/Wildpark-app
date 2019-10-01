52b9e439089cdaca9ee5b49748d742dd2d9c1ee7
elenasmo <elena.smo@web.de>
2019-10-01 18:50:12 +0200

    Add icons

--- src/components/Animal.js
+++ src/components/Animal.js
@@ -1,6 +1,9 @@
 import React, { useState } from 'react'
 import styled from 'styled-components/macro'
 import Information from './Information'
+import { Heart } from 'styled-icons/boxicons-regular/Heart'
+import { KeyboardArrowDown } from 'styled-icons/material/KeyboardArrowDown'
+import { Place } from 'styled-icons/material/Place'
 
 import PropTypes from 'prop-types'
 
@@ -24,10 +27,20 @@ export default function Animal({ title, picture, information }) {
     )
   } else {
     return (
-      <AnimalStyled onClick={toggleInformation}>
-        <img src={picture} alt={title} width="100%" />
-        {isInformationVisible && <Information text={information} />}
-      </AnimalStyled>
+      <>
+        <AnimalStyled onClick={toggleInformation}>
+          <img src={picture} alt={title} width="100%" />
+          <InfoShort>
+            <h3>{title}</h3>
+            <div>
+              <PlaceStyled />
+              <HeartStyled />
+            </div>
+          </InfoShort>
+          <ArrowDownStyled />
+          {isInformationVisible && <Information text={information} />}
+        </AnimalStyled>
+      </>
     )
   }
 }
@@ -36,5 +49,32 @@ const AnimalStyled = styled.section`
 
   border-radius: 5px;
   box-shadow: 0 10px 10px #0002;
-  /* margin: 10px; */
+  position: relative;
+`
+
+const ArrowDownStyled = styled(KeyboardArrowDown)`
+  height: 40px;
+  position: absolute;
+  right: 50%;
+  transform: translateX(+25px);
+  bottom: 1px;
+  color: grey;
+`
+
+const HeartStyled = styled(Heart)`
+  height: 40px;
+  color: plum;
+`
+
+const InfoShort = styled.section`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+`
+
+const PlaceStyled = styled(Place)`
+  height: 40px;
+  color: plum;
 `
--- src/components/AnimalList.js
+++ src/components/AnimalList.js
@@ -2,6 +2,9 @@ import React, { useState } from 'react'
 import styled from 'styled-components/macro'
 import Animal from './Animal'
 import { animals } from '../data/animals'
+import { GridVertical } from 'styled-icons/boxicons-regular/GridVertical'
+import { ViewList } from 'styled-icons/material/ViewList'
+import { Menu } from 'styled-icons/material/Menu'
 
 const animalsEven = animals.filter((animal, index) => index % 2 === 0)
 console.log(animalsEven)
@@ -59,8 +62,18 @@ export default function AnimalList() {
   }
   return (
     <>
-      <button onClick={showGrid}>Grid</button>
-      <button onClick={showFullWidth}>Details</button>
+      <GridStyle>
+        <div>
+          <ButtonStyled onClick={showGrid}>
+            <GridVerticalStyled />
+          </ButtonStyled>
+          <ButtonStyled onClick={showFullWidth}>
+            <ViewListStyled />
+          </ButtonStyled>
+        </div>
+
+        <MenuStyled />
+      </GridStyle>
       {renderArrangement(gridView)}
     </>
   )
@@ -77,5 +90,28 @@ const ColumnStyled = styled.div`
   display: grid;
   grid-auto-rows: min-content;
   grid-gap: 10px;
-  /* padding: 0 4px; */
+`
+const GridStyle = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+`
+
+const ButtonStyled = styled.button`
+  border: none;
+  background-color: white;
+`
+
+const GridVerticalStyled = styled(GridVertical)`
+  height: 35px;
+  color: grey;
+`
+const ViewListStyled = styled(ViewList)`
+  height: 40px;
+  color: grey;
+`
+const MenuStyled = styled(Menu)`
+  height: 45px;
+  color: grey;
 `
