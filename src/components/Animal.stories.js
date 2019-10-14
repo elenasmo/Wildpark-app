e1b8ba63842f0218d2dca67b00fc1b4578824e7d
elenasmo <elena.smo@web.de>
2019-10-14 09:56:41 +0200

    Improve storybook entry

--- src/components/Animal.stories.js
+++ src/components/Animal.stories.js
@@ -1,11 +1,14 @@
 import React from 'react'
 import Animal from './Animal'
 import { withInfo } from '@storybook/addon-info'
+import { text } from '@storybook/addon-knobs/react'
+import { withKnobs } from '@storybook/addon-knobs/dist/deprecated'
+import { action } from '@storybook/addon-actions'
 import schneeleopard from './img/schneeleopard.png'
 
 export default {
   title: 'Animal',
-  decorators: [Wrapper, withInfo]
+  decorators: [Wrapper, withInfo, withKnobs]
 }
 
 function Wrapper(storyFn) {
@@ -13,3 +16,12 @@ function Wrapper(storyFn) {
 }
 
 export const animal = () => <Animal title="Animal" picture={schneeleopard} />
+export const animalWithDetails = () => (
+  <Animal
+    title={text('title', 'Animal')}
+    picture={schneeleopard}
+    information={text('information', 'Hier ist ein schöner Schneeleopard.')}
+    station={text('station', '21')}
+    onLikeClick={action('liked')}
+  />
+)
