83e86d4c6e6ab22e4bbdb0a3198589c970560916
elenasmo <elena.smo@web.de>
2019-10-17 12:03:39 +0200

    Insert icons and change map-style

--- src/components/map/MapPage.js
+++ src/components/map/MapPage.js
@@ -6,13 +6,17 @@ const token = process.env.REACT_APP_MAPBOX_TOKEN
 
 export default function MapPage({ animalList, initAnimal }) {
   const [activeAnimal, setActiveAnimal] = useState(initAnimal)
+
   const [isPopupVisible, setIsPopupVisible] = useState(initAnimal != null)
+
   const [viewport, setViewport] = useState({
     latitude: 53.23756,
     longitude: 10.044543,
     width: '100vw',
     height: '80vh',
-    zoom: 16
+    zoom: 16,
+    maxZoom: 17,
+    minZoom: 15
   })
 
   return (
@@ -22,7 +26,7 @@ export default function MapPage({ animalList, initAnimal }) {
       <ReactMapGl
         {...viewport}
         mapboxApiAccessToken={token}
-        mapStyle="mapbox://styles/mapbox/satellite-v9"
+        mapStyle="mapbox://styles/qesmo/ck1uga55z01ws1do6z6dn2akj"
         onViewportChange={viewport => setViewport(viewport)}
       >
         {animalList.map(animal => (
@@ -31,9 +35,15 @@ export default function MapPage({ animalList, initAnimal }) {
             latitude={parseFloat(animal.latitude)}
             longitude={parseFloat(animal.longitude)}
           >
-            <ButtonStyled onClick={() => showPopup(animal)}>
-              {animal.station}
-            </ButtonStyled>
+            {animal.icon == null ? (
+              <ButtonStyled onClick={() => showPopup(animal)}>
+                {animal.station}
+              </ButtonStyled>
+            ) : (
+              <button onClick={() => showPopup(animal)}>
+                <IconStyled src={animal.icon} alt={animal.title} />
+              </button>
+            )}
           </Marker>
         ))}
         {renderPopup()}
@@ -70,6 +80,11 @@ const ButtonStyled = styled.button`
   text-align: center;
   justify-content: center;
   font-weight: bold;
+  font-size: 1.2em;
+  transform: translateX(-50%);
+`
+const IconStyled = styled.img`
+  width: 40px;
   font-size: 1.2em;
   transform: translateX(-50%);
 `
